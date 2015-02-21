196e09ecc7aa0c573b0501367671da0b4be1ef3a
Nathan Braswell <miloignis@gmail.com>
2015-02-20 20:20:49 -0500

    Changing the types of the images makes it work!

--- src/gbuffer.rs
+++ src/gbuffer.rs
@@ -22,28 +22,48 @@ impl GBuffer {
         unsafe {
             // Create FBO
             gl::GenFramebuffers(1, &mut self.fbo); 
+            println!("Framebuffer {}", self.fbo);
             gl::BindFramebuffer(gl::DRAW_FRAMEBUFFER, self.fbo);
+            //gl::BindFramebuffer(gl::FRAMEBUFFER, self.fbo);
 
             // Create textures
             gl::GenTextures(self.textures.len() as i32, (&mut self.textures[0]) as *mut u32);
             gl::GenTextures(1, &mut self.depth_texture);
+            for i in 0..self.textures.len() {
+                println!("Texture {}", self.textures[i]);
+            }
+            println!("Texture {}", self.depth_texture);
 
             for i in 0..(TextureType::Number as usize) {
                 gl::BindTexture(gl::TEXTURE_2D, self.textures[i]);
-                gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB32F as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::FLOAT, ptr::null());
+                //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB32F as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::FLOAT, ptr::null());
+                //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::BYTE, ptr::null());
+                //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::SHORT, ptr::null());
+                //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::INT, ptr::null());
+                gl::TexImage2D(gl::TEXTURE_2D, 0, gl::RGB as i32, window_width as i32, window_height as i32, 0, gl::RGB, gl::FLOAT, ptr::null());
                 gl::FramebufferTexture2D(gl::DRAW_FRAMEBUFFER, (gl::COLOR_ATTACHMENT0 + i as u32) as u32, gl::TEXTURE_2D, self.textures[i], 0);
             }
             
             gl::BindTexture(gl::TEXTURE_2D, self.depth_texture);
-            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT32F as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::FLOAT, ptr::null());
+            //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT32F as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::FLOAT, ptr::null());
+            //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::BYTE, ptr::null());
+            //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::SHORT, ptr::null());
+            //gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::INT, ptr::null());
+            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::DEPTH_COMPONENT as i32, window_width as i32, window_height as i32, 0, gl::DEPTH_COMPONENT, gl::FLOAT, ptr::null());
             gl::FramebufferTexture2D(gl::DRAW_FRAMEBUFFER, gl::DEPTH_ATTACHMENT, gl::TEXTURE_2D, self.depth_texture, 0);
 
             let draw_buffers = [gl::COLOR_ATTACHMENT0, gl::COLOR_ATTACHMENT1, gl::COLOR_ATTACHMENT2, gl::COLOR_ATTACHMENT3];
             gl::DrawBuffers(draw_buffers.len() as i32, (&draw_buffers[0]) as *const u32);
 
+                println!("glGetError: {}", gl::GetError());
             let status = gl::CheckFramebufferStatus(gl::FRAMEBUFFER);
             if status != gl::FRAMEBUFFER_COMPLETE {
                 println!("Framebuffer creation failed!");
+                println!("glGetError: {}", gl::GetError());
+                println!("status: {}", status);
+                if status == gl::FRAMEBUFFER_UNSUPPORTED {
+                    println!("Framebuffer Unsupported");
+                }
             } else {
                 println!("Framebuffer creation success!");
             }
